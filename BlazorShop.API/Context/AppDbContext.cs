5255794fb62aae9a922b65b2ab32a3c958532ccd
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-06-13 16:30:56 -0300

    finalizaod classe AppDbContext

--- BlazorShop.API/Context/AppDbContext.cs
+++ BlazorShop.API/Context/AppDbContext.cs
@@ -2,11 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorShop.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorShop.API.Context
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Carrinho> Carrinho { get; set; }
+        public DbSet<CarrinhoItem> CarrinhoItem { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        //OnModel Creating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasData(new Categoria
+            {
+                Id = 1,
+                Nome = "Beleza",
+                IconCSS = "Balde"
+            });
+
+            modelBuilder.Entity<Produtos>().HasData(new Produtos
+            {
+                Id = 1,
+                Nome = "Beleza",
+                Descricao = "Balse de Beleza",
+                ImagemUrl = "/Imagens/Beleza/Beleza1.png",
+                Preco = 100,
+                Quantidade = 40,
+                CategoriaId = 1,
+            });
+        }
+
+
     }
 }
