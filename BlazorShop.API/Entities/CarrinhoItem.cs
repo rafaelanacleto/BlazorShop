6be02d9468c04f97cf176a66f29a660c0f4df21c
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-06-13 16:02:26 -0300

    Criado entitie carrinho item

--- BlazorShop.API/Entities/CarrinhoItem.cs
+++ BlazorShop.API/Entities/CarrinhoItem.cs
@@ -7,6 +7,10 @@ namespace BlazorShop.API.Entities
 {
     public class CarrinhoItem
     {
+        public int Id { get; set; }
+        public int CarrinhoId { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
         
     }
 }
