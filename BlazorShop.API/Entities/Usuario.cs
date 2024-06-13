186ed46833e10100ce779e4a7a126c2aae23265a
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-06-13 16:05:58 -0300

    criado entitie usuario

---
+++ BlazorShop.API/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.API.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? NomeUsuario { get; set; }
+    }
+}
