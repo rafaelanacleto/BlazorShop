f270fec10ed29641d25cfee525ba2cf89f8fc4b0
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-06-26 09:35:15 -0300

    Criado camada de DTO

--- BlazorShop.API/Entities/Categoria.cs
+++ BlazorShop.API/Entities/Categoria.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorShop.API.Entities
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [MaxLength(100)] //usado antes de criar as tabelas do banco
         public string Nome { get; set; } = string.Empty;
         public string IconCSS { get; set; } = string.Empty;
 
