c497cb49be522ddb13c649dc475c22210e939efc
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-06-29 21:47:03 -0300

    criando injeção de dependencia do servico de contexto

--- BlazorShop.API/Repositories/ProdutoRepository.cs
+++ BlazorShop.API/Repositories/ProdutoRepository.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorShop.API.Context;
 
 namespace BlazorShop.API.Repositories
 {
     public class ProdutoRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        public ProdutoRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+
+
+
         
     }
 }
