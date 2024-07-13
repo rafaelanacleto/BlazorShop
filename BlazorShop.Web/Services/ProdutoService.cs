5513080be22904d1b23a3b73468e196db5695b1b
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-07-13 10:35:15 -0300

    compilado programa para linux ubuntu

--- BlazorShop.Web/Services/ProdutoService.cs
+++ BlazorShop.Web/Services/ProdutoService.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using BlazorShop.Models.DTOs;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorShop.Web.Services
 {
     public class ProdutoService : IProdutoService
     {
-        private readonly HttpClient _restClient;
+        private readonly HttpClient _restClient;       
+
+
         public ProdutoService(HttpClient restClient)
         {
-            _restClient = restClient;           
+            _restClient = restClient;               
         }
 
         public async Task<IEnumerable<ProdutosDTO>> GetProdutos()
@@ -30,21 +33,33 @@ namespace BlazorShop.Web.Services
             }
         }
 
-        public async Task<ProdutoDTO> GetProdutoById(int id)
+        public async Task<ProdutosDTO> GetProdutoById(int id)
         {
-            try
+             try
             {
-                var prod = await _restClient.GetById(id);
+                var response = await _restClient.GetAsync($"api/Produto/{id}");
 
-                return prod;
+
+                if (response.IsSuccessStatusCode) //status code 200 -  299
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent) // 204
+                    {
+                        return default; //retorna valor padrão / empty
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<ProdutosDTO>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    System.Console.WriteLine("errp");
+                    throw new Exception("Status code");
+                }    
             }
             catch (System.Exception)
             {                
                 throw;
             }
-            
-        }   
-
-
+        }
     }
 }
