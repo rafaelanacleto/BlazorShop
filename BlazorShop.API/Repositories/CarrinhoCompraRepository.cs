b4db03a8cb7b8c171f05fa2acfa22f0a787939a9
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-07-18 19:37:33 -0300

    criando carrinho repository

---
+++ BlazorShop.API/Repositories/CarrinhoCompraRepository.cs
@@ -0,0 +1,33 @@
+﻿using BlazorShop.API.Entities;
+using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.API.Repositories
+{
+    public class CarrinhoCompraRepository : ICarrinhoCompraRepository
+    {
+        public Task<CarrinhoItem> AdicionaItem(CarrinhoItemDTO itemDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> AtualizaQuantidade(int id, CarrinhoItemAtualizaQuantidade carrinhoItemAtualiza)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> DeletaItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CarrinhoItem>> GetItems(string usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorShop.API/Repositories/ICarrinhoCompraRepository.cs
@@ -0,0 +1,15 @@
+﻿using BlazorShop.API.Entities;
+using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.API.Repositories
+{
+    public interface ICarrinhoCompraRepository
+    {
+        Task<CarrinhoItem> AdicionaItem(CarrinhoItemDTO itemDTO);
+        Task<CarrinhoItem> AtualizaQuantidade(int id, CarrinhoItemAtualizaQuantidade carrinhoItemAtualiza);
+        Task<CarrinhoItem> DeletaItem(int id);
+        Task<CarrinhoItem> GetItem(int id);
+        Task<IEnumerable<CarrinhoItem>> GetItems(string usuarioId);
+
+    }
+}
