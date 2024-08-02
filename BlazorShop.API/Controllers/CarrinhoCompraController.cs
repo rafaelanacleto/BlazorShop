d6aac9a66263c5c0f44a2f14afed98b91a9fc7fc
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-08-02 10:01:51 -0300

    Problema na regra de adcionar um item

--- BlazorShop.API/Controllers/CarrinhoCompraController.cs
+++ BlazorShop.API/Controllers/CarrinhoCompraController.cs
@@ -78,7 +78,7 @@ namespace BlazorShop.API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CarrinhoItemDTO>> PostItem([FromBody] CarrinhoItemAdicionaDTO carrinho)
+        public async Task<ActionResult<CarrinhoItemDTO>> PostItem(CarrinhoItemAdicionaDTO carrinho)
         {
             try
             {
