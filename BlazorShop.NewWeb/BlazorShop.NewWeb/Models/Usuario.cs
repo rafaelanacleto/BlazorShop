087b4547d8082e109f20316d672551fd7e8728d8
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-10-02 13:26:35 -0300

    Finalizando validação de formulário

--- BlazorShop.NewWeb/BlazorShop.NewWeb/Models/Usuario.cs
+++ BlazorShop.NewWeb/BlazorShop.NewWeb/Models/Usuario.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace BlazorShop.NewWeb.Models
 {
     public class Usuario
     {
+        [Required(ErrorMessage = "Email é requirido")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
         public string? Email { get; set; }
         public string? Senha { get; set; }
         
